004eea866a9be498697724517886f3eaae27227b
JuanEMetro <jencizo@metrolinkeu.com>
2024-08-16 16:55:06 -0500

    Se ajuta la zona horaria par que guarde los datos con hora de bogota.    se crea un metodo provicional para ejecutar tareas programadas, a la hora de crear una agenda.  Se modifica el properties para que la aplicacion tenga la hora de bogota

--- src/main/java/com/metrolink/ami_api/services/procesos/programacionesAmi/AgendaProgramacionesAMIService.java
+++ src/main/java/com/metrolink/ami_api/services/procesos/programacionesAmi/AgendaProgramacionesAMIService.java
@@ -1,12 +1,21 @@
 package com.metrolink.ami_api.services.procesos.programacionesAmi;
 
 import com.metrolink.ami_api.models.procesos.programacionesAmi.AgendaProgramacionesAMI;
+import com.metrolink.ami_api.models.procesos.programacionesAmi.ProgramacionesAMI;
 import com.metrolink.ami_api.repositories.procesos.programacionesAmi.AgendaProgramacionesAMIRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import javax.persistence.EntityNotFoundException;
 import javax.transaction.Transactional;
+
+import java.sql.Timestamp;
+import java.time.Duration;
+import java.time.Instant;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
 import java.util.List;
 
 @Service
@@ -17,6 +26,9 @@ public class AgendaProgramacionesAMIService {
 
     @Autowired
     private AsignacionAgendaAMedidoresService asignacionAgendaAMedidores;
+
+    @Autowired
+    private ProgramacionesAMIService programacionesAMIService;
 
     @Transactional
     public AgendaProgramacionesAMI save(AgendaProgramacionesAMI agendaProgramacionesAMI, boolean isUpdate) {
@@ -29,6 +41,30 @@ public class AgendaProgramacionesAMIService {
 
         // Verificar y procesar la agenda mediante AsignacionAgendaAMedidores
         asignacionAgendaAMedidores.verificarYProcesar(agenda);
+
+        // Programar la tarea para pintar "Hola Mundo" en pantalla
+
+
+        Long ncodigo = agenda.getProgramacionAMI().getNcodigo();
+        System.out.println(ncodigo);
+
+        ProgramacionesAMI programacionAMI = programacionesAMIService.findById(agenda.getProgramacionAMI().getNcodigo());
+
+        Timestamp tiempoInicio = programacionAMI.getParametrizacionProg().getDfechaHoraInicio();
+
+        System.out.println(tiempoInicio);
+
+        Instant ahora = Instant.now();
+        long delay = Duration.between(ahora, tiempoInicio.toInstant()).toMillis();
+
+        if (delay > 0) {
+            ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(1);
+            scheduler.schedule(() -> {
+                System.out.println("Hola Mundo");
+            }, delay, TimeUnit.MILLISECONDS);
+        } else {
+            System.out.println("La fecha y hora ya han pasado, no se puede programar la tarea.");
+        }
 
         return agenda;
     }
